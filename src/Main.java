e8b1c4f7d052c571fe24846be4d20a2960d8be37
efernandezcostas <efernandezcostas@danielcastelao.org>
2024-04-25 14:48:01 +0200

    clases principales

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
+import usuarios.GestorUsuarios;
+import usuarios.Usuario;
+
 public class Main {
     public static void main(String[] args) {
-        GestorUsuarios gestorUsuarios = new GestorUsuarios();
-        Usuario usuario = new Usuario("Pepe", "pepe");
-        System.out.println( gestorUsuarios.getListaUsuarios().add(usuario) );
-        System.out.println( gestorUsuarios.eliminarUsuario(usuario) );
-        System.out.println(usuario);
+
     }
 }
