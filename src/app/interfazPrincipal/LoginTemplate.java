903bdfc8fb161240e54188e464f9a87ab6491507
PabloIglesiasL <piglesiasleyenda@danielcastelao.org>
2024-04-25 10:21:48 +0200

    Añadidas imagenes de usuario, contraseña, cierre y corregida fuente

--- src/app/interfazPrincipal/LoginTemplate.java
+++ src/app/interfazPrincipal/LoginTemplate.java
@@ -3,12 +3,10 @@ package app.interfazPrincipal;
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
-import java.io.IOException;
-import java.io.InputStream;
 
 public class LoginTemplate extends JFrame{
     /* Declaracion elementos */
-    private JLabel tituloLogin;
+    private JLabel textoLogin;
     private JTextField cuadroUsuario;
     private JPasswordField cuadroPassword;
     private JButton botonEntrar, botonCerrar, botonRegistrar;
@@ -16,8 +14,9 @@ public class LoginTemplate extends JFrame{
 
     /* Declaracion colores y fuentes */
     private Color GRANATE = new Color(82,0,0);
-    private Font MONSERRAT_BIG = new Font("Montserrat", Font.PLAIN, 22);
-    private Font MONSERRAT_SMALL = new Font("Montserrat", Font.PLAIN, 13);
+    private Font ArialDefault = new Font("Arial", Font.PLAIN, 14);
+    private Font ArialTitle = new Font("Arial", Font.BOLD, 24);
+    private Font ArialBold = new Font("Arial", Font.BOLD, 13);
 
     /* Declaracion tipo de cursor y bordes */
     private Cursor cursorMano = new Cursor(Cursor.HAND_CURSOR);
@@ -28,11 +27,9 @@ public class LoginTemplate extends JFrame{
     private JLabel labelCerrar = new JLabel();
     private JLabel labelUsuario = new JLabel();
     private JLabel labelPassword = new JLabel();
-    private ImageIcon imagenLogo, imagenCerrar, imagenUsuario, imagenPassword, DimAux;
+    private ImageIcon imagenLogo, imagenCerrar, imagenUsuario, imagenPassword;
 
     public LoginTemplate(){
-        this.generarFuente(); // Generar la fuente
-
         /* Cargar imágenes */
         imagenLogo = new ImageIcon("resources/images/logo1.png");
         imagenCerrar = new ImageIcon("resources/images/close.png");
@@ -47,7 +44,7 @@ public class LoginTemplate extends JFrame{
         panelIzquierda.setLayout(null);
 
         labelLogo.setIcon(imagenLogo);
-        labelLogo.setBounds(50, 100, 550, 250);
+        labelLogo.setBounds(50, 110, 550, 250);
 
         /* Parte de derecha de la interfaz */
         panelDerecha=new JPanel();
@@ -56,6 +53,13 @@ public class LoginTemplate extends JFrame{
         panelDerecha.setBackground(Color.WHITE);
         panelDerecha.setLayout(null);
 
+        textoLogin = new JLabel("Iniciar Sesión");
+        textoLogin.setSize(400, 80);
+        textoLogin.setLocation((panelDerecha.getWidth() - textoLogin.getWidth()) / 2, 65);
+        textoLogin.setForeground(GRANATE);
+        textoLogin.setFont(ArialTitle);
+        textoLogin.setHorizontalAlignment(SwingConstants.CENTER);
+
         cuadroUsuario=new JTextField();
         cuadroUsuario.setSize(260, 40);
         cuadroUsuario.setLocation((panelDerecha.getWidth() - cuadroUsuario.getWidth()) / 2, 150); // Centrado en el panel de la derecha
@@ -63,7 +67,7 @@ public class LoginTemplate extends JFrame{
         cuadroUsuario.setBackground(Color.WHITE); // Color de fondo
         cuadroUsuario.setCaretColor(GRANATE); // Color de la línea del cursor
         cuadroUsuario.setHorizontalAlignment(SwingConstants.CENTER); // Para que el texto se empiece a escribir en el centro
-        cuadroUsuario.setFont(MONSERRAT_SMALL);
+        cuadroUsuario.setFont(ArialBold);
         cuadroUsuario.setBorder(borde); // Borde personalizado
 
         cuadroPassword =new JPasswordField();
@@ -81,54 +85,64 @@ public class LoginTemplate extends JFrame{
         botonEntrar.setBackground(GRANATE);
         botonEntrar.setForeground(Color.WHITE);
         botonEntrar.setFocusable(false); // Para que no se vea el cuadro de selección en el texto
-        botonEntrar.setFont(MONSERRAT_SMALL);
+        botonEntrar.setFont(ArialDefault);
         botonEntrar.setCursor(cursorMano); // Cambiar el cursor
 
-        botonCerrar = new JButton("X");
+        /*botonCerrar = new JButton("X");
         botonCerrar.setBounds(330, 10, 45, 30);
         botonCerrar.setFocusable(false);
         botonCerrar.setBackground(GRANATE);
         botonCerrar.setForeground(Color.WHITE);
-        botonCerrar.setFont(MONSERRAT_SMALL);
-        botonCerrar.setCursor(cursorMano);
+        botonCerrar.setFont(ArialBold);
+        botonCerrar.setCursor(cursorMano);*/
 
         botonRegistrar = new JButton("Registrarse");
-        botonRegistrar.setSize(145, 35);
-        botonRegistrar.setLocation((panelDerecha.getWidth() - botonEntrar.getWidth())-20, 380);
+        botonRegistrar.setSize(150, 40);
+        botonRegistrar.setLocation((panelDerecha.getWidth() - botonEntrar.getWidth())-25, 370);
         botonRegistrar.setBackground(GRANATE);
         botonRegistrar.setForeground(Color.WHITE);
         botonRegistrar.setFocusable(false);
-        botonRegistrar.setFont(MONSERRAT_SMALL);
+        botonRegistrar.setFont(ArialDefault);
         botonRegistrar.setCursor(cursorMano);
+
+        labelCerrar.setIcon(imagenCerrar);
+        labelCerrar.setBounds(360, 5, 40, 40);
+        labelCerrar.setFocusable(false);
+        labelCerrar.setBorder(null);
+        labelCerrar.setCursor(cursorMano);
+
+        labelPassword.setIcon(imagenPassword);
+        labelPassword.setBounds(25, 230, 32, 32);
+        labelPassword.setFocusable(false);
+        labelPassword.setBorder(null);
+
+        labelUsuario.setIcon(imagenUsuario);
+        labelUsuario.setBounds(25, 160, 32, 32);
+        labelUsuario.setFocusable(false);
+        labelUsuario.setBorder(null);
 
         /* AÑADIR BOTONES */
         this.add(panelDerecha);
         this.add(panelIzquierda);
+        panelDerecha.add(textoLogin);
         panelDerecha.add(cuadroUsuario);
         panelDerecha.add(cuadroPassword);
         panelDerecha.add(botonEntrar);
-        panelDerecha.add(botonCerrar);
+        /*panelDerecha.add(botonCerrar);*/
         panelDerecha.add(botonRegistrar);
+        panelDerecha.add(labelCerrar);
+        panelDerecha.add(labelUsuario);
+        panelDerecha.add(labelPassword);
         panelIzquierda.add(labelLogo);
 
         /* CONFIGURACIÓN DE LA VENTANA */
-        setTitle("Interfaz Principal");
+        setTitle("BLOOM - Login");
         setLayout(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(1000, 500);
         setLocationRelativeTo(this);
+        setResizable(false);
+        /*setUndecorated(true);*/
         setVisible(true);
-    }
-
-    /* Método para generar la fuente */
-    private void generarFuente() {
-        try {
-            InputStream is = getClass().getResourceAsStream("/resources/fonts/Montserrat.ttf"); // Cargar fuente independientemente del sistema operativo
-            Font font = Font.createFont(Font.TRUETYPE_FONT, is); // Crea fuente y le da formato (TRUETYPE no altera la fuente)
-            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-            ge.registerFont(font); // Registra la fuente
-        } catch (IOException | FontFormatException e) { // Excepcion por si no se encuentra la fuente
-            System.out.println(e);
-        }
     }
 }
