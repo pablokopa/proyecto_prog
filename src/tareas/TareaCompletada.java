182477e6d8cbd8173a3e9d50a866c5196c7bb7aa
efernandezcostas <efernandezcostas@danielcastelao.org>
2024-04-26 14:22:16 +0200

    añadidas datas de comienzo y finalizacion de tareas

--- src/tareas/TareaCompletada.java
+++ src/tareas/TareaCompletada.java
@@ -1,9 +1,13 @@
 package tareas;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
 public class TareaCompletada extends Tarea{
-    private String dataFinalizacion;
+    private ZonedDateTime dataFinalizacion;
 
     public TareaCompletada(String nombreTarea, String descripcionTarea) {
         super(nombreTarea, descripcionTarea);
+        this.dataFinalizacion = ZonedDateTime.now(ZoneId.of("Europe/Madrid"));
     }
 }
--- src/tareas/TareaToDo.java
+++ src/tareas/TareaToDo.java
@@ -1,6 +1,10 @@
 package tareas;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
 public class TareaToDo extends Tarea{
+    private ZonedDateTime fechaCreacion;
 
     /**
      * Constructor, recibe un ID único desde la clase padre
@@ -9,6 +13,7 @@ public class TareaToDo extends Tarea{
      */
     public TareaToDo(String nombreTarea, String descripcionTarea) {
         super(nombreTarea, descripcionTarea);
+        this.fechaCreacion = ZonedDateTime.now(ZoneId.of("Europe/Madrid"));      // añade la fecha de creación y hora basada en la zona horaria de España
     }
 
 
