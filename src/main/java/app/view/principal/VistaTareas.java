b200829178998d85c83488880dede4c33973e2c7
efernandezcostas <efernandezcostas@danielcastelao.org>
2024-05-18 11:47:10 +0200

    cambiada toda VistaTareas

--- src/main/java/app/view/principal/VistaTareas.java
+++ src/main/java/app/view/principal/VistaTareas.java
@@ -15,109 +15,157 @@ import java.util.ArrayList;
 
 public class VistaTareas extends JPanel {
     private final Recursos sRecursos = Recursos.getService();
-    private GridBagConstraints gbc;
-    private GestorTareas gestorTareas;
-    private Usuario usuario;
+    private final GridBagConstraints gbc;
+    private final GestorTareas gestorTareas;
+    private final Usuario usuario;
 
-    private JPanel panelTareasToDo, panelTareasCompletadas, panelInformacionTarea, panelListaTareasToDo, panelListaTareasCompletadas;
-    private JLabel labelTituloListaTareasToDo, labelTituloInformacionTarea, labelCrearNuevaTarea, labelTituloListaTareasCompletadas;
+    private JPanel panelColumnaTareasToDo, panelColumnaTareasCompletadas, panelColumnaInformacionExtra;
+    private JPanel panelListaTareasToDo, panelListaTareasCompletadas, panelInformacionNuevaTarea;
+    private JScrollPane scroolPanelListaTareasToDo, scroolPanelListaTareasCompletadas;
+    private JLabel labelTituloTareasToDo, labelCrearNuevaTarea, labelTituloTareasCompletadas, labelTituloInformacionNuevaTarea;
+
+    private final CardLayout cardLayout;
 
     public VistaTareas(GestorTareas gestorTareas) {
         this.gestorTareas = gestorTareas;
         this.usuario = gestorTareas.getUsuario();
+        this.cardLayout = new CardLayout();
 
         this.setLayout(new GridBagLayout());
         gbc = new GridBagConstraints();
 
-        crearPaneles();
-        crearLabels();
+        construirColumnaTareasToDo();
+        construirColumnaTareasCompletadas();
+        construirColumnaInformacionExtra();
         addTareas();
     }
 
     public void crearPaneles() {
-        this.panelTareasToDo = new JPanel();
-        panelTareasToDo.setLayout(new BorderLayout());
-        panelTareasToDo.setBorder(new MatteBorder(0, 10, 10, 5, sRecursos.getBLANCO()));
-        panelTareasToDo.setBackground(Color.GREEN);
+    }
+
+    private void construirColumnaTareasToDo() {
+        /* Columna Tareas To Do */
+        this.panelColumnaTareasToDo = new JPanel();
+        panelColumnaTareasToDo.setLayout(new BorderLayout());
+        panelColumnaTareasToDo.setBorder(new MatteBorder(0, 10, 10, 5, sRecursos.getBLANCO()));
+        panelColumnaTareasToDo.setBackground(Color.GREEN);
+        panelColumnaTareasToDo.setPreferredSize(new Dimension(200, 0));
         gbc.gridx = 0;      // Columna 1
         gbc.gridy = 0;      // Fila 0
-        gbc.weightx = 0.2;  // Ocupa x% del espacio horizontal
+        gbc.weightx = 0.3;  // Ocupa x% del espacio horizontal
         gbc.weighty = 1;    // Ocupa y% del espacio vertical
         gbc.fill = GridBagConstraints.BOTH;     // Se expande en ambas direcciones
-        add(panelTareasToDo, gbc);
+        add(panelColumnaTareasToDo, gbc);
 
-        this.panelTareasCompletadas = new JPanel();
-        panelTareasCompletadas.setLayout(new BorderLayout());
-        panelTareasCompletadas.setBorder(new MatteBorder(0, 5, 10, 5, sRecursos.getBLANCO()));
-        panelTareasCompletadas.setBackground(Color.RED);
-        gbc.gridx = 1;      // Columna 2
-        gbc.gridy = 0;      // Fila 0
-        gbc.weightx = 0.2;  // Ocupa x% del espacio horizontal
-        gbc.weighty = 1;    // Ocupa y% del espacio vertical
-        gbc.fill = GridBagConstraints.BOTH;     // Se expande en ambas direcciones
-        add(panelTareasCompletadas, gbc);
+        /* Título de la columna to do */
+        this.labelTituloTareasToDo = new JLabel("Tareas por hacer");
+        labelTituloTareasToDo.setFont(sRecursos.getMonserratBold(25));
+        labelTituloTareasToDo.setHorizontalAlignment(SwingConstants.CENTER);
+        labelTituloTareasToDo.setBackground(Color.lightGray);
+        labelTituloTareasToDo.setOpaque(true);
+        panelColumnaTareasToDo.add(labelTituloTareasToDo, BorderLayout.NORTH);
 
-        // Los parámetros de gbc no sobrescritos también se aplican aquí (todos excepto gridx y weightx)
-        this.panelInformacionTarea = new JPanel();
-        panelInformacionTarea.setLayout(new BorderLayout());
-        panelInformacionTarea.setBackground(Color.BLUE);
-        panelInformacionTarea.setBorder(new MatteBorder(0, 5, 10, 10, sRecursos.getBLANCO()));
-        gbc.gridx = 2;          // Columna 3
-        gbc.gridy = 0;          // Fila 0
-        gbc.weightx = 0.6;      // Ocupa x% del espacio horizontal
-        gbc.weighty = 1;        // Ocupa y% del espacio vertical
-        gbc.fill = GridBagConstraints.BOTH;     // Se expande en ambas direcciones
-        add(panelInformacionTarea, gbc);
-
+        /* Panel donde están las tareas to do y convertido a JScrollPane */
         this.panelListaTareasToDo = new JPanel();
         panelListaTareasToDo.setLayout(new BoxLayout(panelListaTareasToDo, BoxLayout.Y_AXIS));
         panelListaTareasToDo.setBackground(Color.yellow);
-        panelTareasToDo.add(panelListaTareasToDo, BorderLayout.CENTER);
 
-        this.panelListaTareasCompletadas = new JPanel();
-        panelListaTareasCompletadas.setLayout(new BoxLayout(panelListaTareasCompletadas, BoxLayout.Y_AXIS));
-        panelListaTareasCompletadas.setBackground(Color.yellow);
-        panelTareasCompletadas.add(panelListaTareasCompletadas, BorderLayout.CENTER);
-    }
+        this.scroolPanelListaTareasToDo = new JScrollPane(panelListaTareasToDo);
+        scroolPanelListaTareasToDo.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        scroolPanelListaTareasToDo.setBorder(null);
+        panelColumnaTareasToDo.add(scroolPanelListaTareasToDo, BorderLayout.CENTER);
 
-    private void crearLabels() {
-        this.labelTituloListaTareasToDo = new JLabel("Tareas por hacer");
-        labelTituloListaTareasToDo.setFont(sRecursos.getMonserratBold(25));
-        labelTituloListaTareasToDo.setHorizontalAlignment(SwingConstants.CENTER);
-        labelTituloListaTareasToDo.setBackground(Color.lightGray);
-        labelTituloListaTareasToDo.setOpaque(true);
-        panelTareasToDo.add(labelTituloListaTareasToDo, BorderLayout.NORTH);
-
-        this.labelTituloListaTareasCompletadas = new JLabel("Tareas completadas");
-        labelTituloListaTareasCompletadas.setFont(sRecursos.getMonserratBold(25));
-        labelTituloListaTareasCompletadas.setHorizontalAlignment(SwingConstants.CENTER);
-        labelTituloListaTareasCompletadas.setBackground(Color.lightGray);
-        labelTituloListaTareasCompletadas.setOpaque(true);
-        panelTareasCompletadas.add(labelTituloListaTareasCompletadas, BorderLayout.NORTH);
-
+        /* Label para crear nueva tarea */
         this.labelCrearNuevaTarea = new JLabel("Crear nueva tarea");
         labelCrearNuevaTarea.setFont(sRecursos.getMonserratBold(25));
         labelCrearNuevaTarea.setHorizontalAlignment(SwingConstants.CENTER);
         labelCrearNuevaTarea.setBackground(Color.lightGray);
         labelCrearNuevaTarea.setOpaque(true);
-        panelTareasToDo.add(labelCrearNuevaTarea, BorderLayout.SOUTH);
+        panelColumnaTareasToDo.add(labelCrearNuevaTarea, BorderLayout.SOUTH);
 
-        this.labelTituloInformacionTarea = new JLabel("Información");
-        labelTituloInformacionTarea.setFont(sRecursos.getMonserratBold(25));
-        labelTituloInformacionTarea.setHorizontalAlignment(SwingConstants.CENTER);
-        labelTituloInformacionTarea.setBackground(Color.lightGray);
-        labelTituloInformacionTarea.setOpaque(true);
-        panelInformacionTarea.add(labelTituloInformacionTarea, BorderLayout.NORTH);
+        labelCrearNuevaTarea.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+//                gestorTareas.crearTarea();
+            }
+        });
     }
 
+    private void construirColumnaTareasCompletadas() {
+        /* Columna Tareas Completadas */
+        this.panelColumnaTareasCompletadas = new JPanel();
+        panelColumnaTareasCompletadas.setLayout(new BorderLayout());
+        panelColumnaTareasCompletadas.setBorder(new MatteBorder(0, 5, 10, 5, sRecursos.getBLANCO()));
+        panelColumnaTareasCompletadas.setBackground(Color.RED);
+        panelColumnaTareasCompletadas.setPreferredSize(new Dimension(200, 0));
+        gbc.gridx = 1;      // Columna 2
+        gbc.gridy = 0;      // Fila 0
+        gbc.weightx = 0.3;  // Ocupa x% del espacio horizontal
+        gbc.weighty = 1;    // Ocupa y% del espacio vertical
+        gbc.fill = GridBagConstraints.BOTH;     // Se expande en ambas direcciones
+        add(panelColumnaTareasCompletadas, gbc);
+
+        /* Título de la columna completadas */
+        this.labelTituloTareasCompletadas = new JLabel("Tareas completadas");
+        labelTituloTareasCompletadas.setFont(sRecursos.getMonserratBold(25));
+        labelTituloTareasCompletadas.setHorizontalAlignment(SwingConstants.CENTER);
+        labelTituloTareasCompletadas.setBackground(Color.lightGray);
+        labelTituloTareasCompletadas.setOpaque(true);
+        panelColumnaTareasCompletadas.add(labelTituloTareasCompletadas, BorderLayout.NORTH);
+
+        /* Panel donde están las tareas completadas y convertido a JScrollPane */
+        this.panelListaTareasCompletadas = new JPanel();
+        panelListaTareasCompletadas.setLayout(new BoxLayout(panelListaTareasCompletadas, BoxLayout.Y_AXIS));
+        panelListaTareasCompletadas.setBackground(Color.yellow);
+
+        this.scroolPanelListaTareasCompletadas = new JScrollPane(panelListaTareasCompletadas);
+        scroolPanelListaTareasCompletadas.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        scroolPanelListaTareasCompletadas.setBorder(null);
+        panelColumnaTareasCompletadas.add(scroolPanelListaTareasCompletadas, BorderLayout.CENTER);
+    }
+
+    private void construirColumnaInformacionExtra() {
+        /* Columna Información Extra */
+        this.panelColumnaInformacionExtra = new JPanel();
+        panelColumnaInformacionExtra.setLayout(cardLayout);
+        panelColumnaInformacionExtra.setBackground(Color.BLUE);
+        panelColumnaInformacionExtra.setBorder(new MatteBorder(0, 5, 10, 10, sRecursos.getBLANCO()));
+        panelColumnaInformacionExtra.setPreferredSize(new Dimension(200, 0));
+        gbc.gridx = 2;          // Columna 3
+        gbc.gridy = 0;          // Fila 0
+        gbc.weightx = 0.4;      // Ocupa x% del espacio horizontal
+        gbc.weighty = 1;        // Ocupa y% del espacio vertical
+        gbc.fill = GridBagConstraints.BOTH;     // Se expande en ambas direcciones
+        add(panelColumnaInformacionExtra, gbc);
+
+        /* Título de la columna información extra */
+        this.labelTituloInformacionNuevaTarea = new JLabel("Nueva tarea");
+        labelTituloInformacionNuevaTarea.setFont(sRecursos.getMonserratBold(25));
+        labelTituloInformacionNuevaTarea.setHorizontalAlignment(SwingConstants.CENTER);
+        labelTituloInformacionNuevaTarea.setBackground(Color.lightGray);
+        labelTituloInformacionNuevaTarea.setOpaque(true);
+        panelInformacionNuevaTarea.add(labelTituloInformacionNuevaTarea, BorderLayout.NORTH);
+
+        /* Panel donde se muestra la información de la tarea seleccionada */
+        this.panelInformacionNuevaTarea = new JPanel();
+        panelInformacionNuevaTarea.setLayout(new BorderLayout());
+        panelInformacionNuevaTarea.setBackground(Color.orange);
+        panelColumnaInformacionExtra.add(panelInformacionNuevaTarea, "Crear nueva tarea");
+    }
+
+    /**
+     * Recupera las tareas del usuario de la base de datos, y les añade las funciones (Listener) necesarias
+     */
     private void addTareas() {
+        /* Obtiene las tareas del usuario de la base de datos y las guarda en un ArrayList */
         gestorTareas.getTareasDeBase();
         ArrayList<Tarea> listaTareas = gestorTareas.getListaTareas();
 
+        /* Recorre la lista de tareas y utiliza el template PanelTareaEspecífica para mostrarlas */
         for (Tarea tarea : listaTareas) {
             PanelTareaEspecifica panelTarea = new PanelTareaEspecifica(tarea, gestorTareas);
 
-            /* Completa la tarea al hacer click en el check y lo cambia en consecuencia */
+            /* Añade un MouseListener al icono del check que completa la tarea al hacer click sobre él y cambia la tarea de columna */
             panelTarea.getLabelImagen().addMouseListener(new MouseAdapter() {
                 @Override
                 public void mouseClicked(MouseEvent e) {
@@ -130,7 +178,6 @@ public class VistaTareas extends JPanel {
                             panelTarea.getLabelImagen().setIcon(sRecursos.getImagenCheckSinCheck());
                             panelListaTareasToDo.add(panelTarea);
                             panelListaTareasCompletadas.remove(panelTarea);
-
                         }
                     }
                     repaint();
@@ -146,6 +193,7 @@ public class VistaTareas extends JPanel {
                 }
             });
 
+            /* Al iniciar la aplicación; si la tarea está completada la añade al panel de tareas completadas, si no, la añade al panel de tareas por hacer */
             if (tarea.getCompletadaT()){
                 panelListaTareasCompletadas.add(panelTarea);
             } else {
