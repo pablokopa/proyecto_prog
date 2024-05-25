0fb741ed8b3907ba2d64ec716de637c92eafa0a4
efernandezcostas <efernandezcostas@danielcastelao.org>
2024-05-25 10:02:23 +0200

    hecha clase scrollbar interna, cambiado cursor boton añadir tareas

--- src/main/java/app/view/principal/VistaMatrix.java
+++ src/main/java/app/view/principal/VistaMatrix.java
@@ -3,7 +3,6 @@ package app.view.principal;
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
-import javax.swing.border.Border;
 import javax.swing.border.MatteBorder;
 import javax.swing.plaf.basic.BasicScrollBarUI;
 
@@ -15,10 +14,10 @@ import services.Recursos;
 public class VistaMatrix extends JPanel {
     private Recursos sRecursos;
     GestorTareas gestorTareas = new GestorTareas();
-    VistaTareas tareas = new VistaTareas(gestorTareas);
+//    VistaTareas tareas = new VistaTareas(gestorTareas);
 
     private Color colorGrisPrincipal = new Color(59, 59, 59);
-    private Color colorGrisSecundario = new Color(220, 220, 220);
+//    private Color colorGrisSecundario = new Color(220, 220, 220);
     private Color colorVerde = new Color(175, 255, 168);
     private Color colorAmarillo = new Color(255, 255, 168);
     private Color colorRojo = new Color(255, 168, 168);
@@ -80,20 +79,21 @@ public class VistaMatrix extends JPanel {
         botonAddTareas.setBorder(new MatteBorder(5, 5, 5, 5, colorGrisPrincipal));
         botonAddTareas.setBackground(colorGrisPrincipal);
         botonAddTareas.setForeground(color);
+        botonAddTareas.setCursor(sRecursos.getCursorMano());
 
-        botonAddTareas.addActionListener(e -> {
-            // Obtener los paneles de tareas por hacer
-            ArrayList<TemplatePanelTareaEspecifica> panelesTareasToDo = tareas.getListaPanelesTareasToDo();
-
-            // Añadir cada panel de tarea al panel
-            for (TemplatePanelTareaEspecifica panelTarea : panelesTareasToDo) {
-                panelTareas.add(panelTarea);
-            }
-
-            // Actualizar el panel
-            panelTareas.revalidate();
-            panelTareas.repaint();
-        });
+//        botonAddTareas.addActionListener(e -> {
+//            // Obtener los paneles de tareas por hacer
+//            ArrayList<TemplatePanelTareaEspecifica> panelesTareasToDo = tareas.getListaPanelesTareasToDo();
+//
+//            // Añadir cada panel de tarea al panel
+//            for (TemplatePanelTareaEspecifica panelTarea : panelesTareasToDo) {
+//                panelTareas.add(panelTarea);
+//            }
+//
+//            // Actualizar el panel
+//            panelTareas.revalidate();
+//            panelTareas.repaint();
+//        });
 
         panelTituloBoton.add(botonAddTareas);
 
@@ -117,39 +117,29 @@ public class VistaMatrix extends JPanel {
         panelAbajoD.setBorder(new MatteBorder(5, 5, 10, 10, sRecursos.getBLANCO()));
         add(panelAbajoD);
     }
-}
 
-class ScrollBarBlanco extends BasicScrollBarUI {
-    @Override
-    protected JButton createDecreaseButton(int orientation) {
-        JButton button = new JButton();
-        button.setPreferredSize(new Dimension(0, 0));
-        button.setMinimumSize(new Dimension(0, 0));
-        button.setMaximumSize(new Dimension(0, 0));
-        button.setOpaque(false);
-        button.setContentAreaFilled(false);
-        button.setBorderPainted(false);
-        return button;
-    }
+    class ScrollBarBlanco extends BasicScrollBarUI {
+        @Override
+        protected JButton createDecreaseButton(int orientation) {
+            JButton button = new JButton();
+            button.setPreferredSize(new Dimension(0, 0));
+            return button;
+        }
 
-    @Override
-    protected JButton createIncreaseButton(int orientation) {
-        JButton button = new JButton();
-        button.setPreferredSize(new Dimension(0, 0));
-        button.setMinimumSize(new Dimension(0, 0));
-        button.setMaximumSize(new Dimension(0, 0));
-        button.setOpaque(false);
-        button.setContentAreaFilled(false);
-        button.setBorderPainted(false);
-        return button;
-    }
+        @Override
+        protected JButton createIncreaseButton(int orientation) {
+            JButton button = new JButton();
+            button.setPreferredSize(new Dimension(0, 0));
+            return button;
+        }
 
-    @Override
-    protected void configureScrollBarColors() {
-        this.thumbColor = new Color(59,59,59);
-        this.trackColor = new Color(220, 220, 220);
-        this.thumbDarkShadowColor = new Color(0, 0, 0, 0);
-        this.thumbHighlightColor = new Color(0, 0, 0, 0);
-        this.thumbLightShadowColor = new Color(0, 0, 0, 0);
+        @Override
+        protected void configureScrollBarColors() {
+            this.thumbColor = new Color(59,59,59);
+            this.trackColor = new Color(220, 220, 220);
+            this.thumbDarkShadowColor = new Color(0, 0, 0, 0);
+            this.thumbHighlightColor = new Color(0, 0, 0, 0);
+            this.thumbLightShadowColor = new Color(0, 0, 0, 0);
+        }
     }
 }
