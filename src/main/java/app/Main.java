837f4e1201f463fb1b93538d3a924b6fcdc30c30
efernandezcostas <efernandezcostas@danielcastelao.org>
2024-05-26 14:33:32 +0200

    cambiado main

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,17 +1,11 @@
 package app;
 
-import app.model.tareas.GestorTareas;
-import app.model.usuarios.Usuario;
 import app.view.login.InterfazLogin;
 import app.model.usuarios.GestorUsuarios;
-import app.view.principal.InterfazPrincipal;
 
 public class Main {
     public static void main(String[] args) {
         GestorUsuarios gestorUsuarios = new GestorUsuarios();
-//        InterfazLogin interfazLogin = new InterfazLogin(gestorUsuarios);
-        Usuario.setUsuarioConectado("pep", "pepe");
-        new InterfazPrincipal(new GestorTareas());
-
+        InterfazLogin interfazLogin = new InterfazLogin(gestorUsuarios);
     }
 }
